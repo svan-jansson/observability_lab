1da95ac127d7e1f4fe03ab2c6a0e3c11a5457c6f
Erik N Svensson <erik.n.svensson@betssongroup.com>
2020-11-12 11:48:13 +0100

    feat(loki): add simple project that logs to Loki

---
+++ cli/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Sinks.Loki;
+using System;
+
+namespace Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var credentials = new NoAuthCredentials("http://localhost:3100"); // Address to local or remote Loki server
+
+            Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .Enrich.FromLogContext()
+                    .WriteTo.LokiHttp(credentials)
+                    .CreateLogger();
+
+            var position = new { Latitude = 25, Longitude = 134 };
+            var elapsedMs = 34;
+            Log.Information("Message processed {@Position} in {Elapsed:000} ms.", position, elapsedMs);
+
+            Log.CloseAndFlush();
+        }
+    }
+}
