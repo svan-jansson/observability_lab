7338d2ce6b2a8cf942f90916ff8958619c1bfd2c
Svan Jansson <svan.jansson@gmail.com>
2020-11-19 20:40:04 +0100

    feat: add script to start all services

---
+++ start-services.sh
@@ -0,0 +1,3 @@
+dotnet run --project ./CurrencyConversionService/CurrencyConversionService.csproj & \
+dotnet run --project ./CurrencyLookupService/CurrencyLookupService.csproj & \
+dotnet run --project ./CurrencyApi/CurrencyApi.csproj
