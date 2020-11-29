a01000d31b26d65486bca087d5a0006ea2d8d091
Svan Jansson <svan.jansson@gmail.com>
2020-11-29 20:09:07 +0100

    feat(codeexamples): add console examples for tracing and metrics

---
+++ code_examples/Metrics/Program.cs
@@ -0,0 +1,46 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Exporter.Prometheus;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Metrics.Export;
+using OpenTelemetry.Trace;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Metrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 9999;
+            var promOptions = new PrometheusExporterOptions() { Url = $"http://localhost:{port}/metrics/" };
+            var promExporter = new PrometheusExporter(promOptions);
+            var metricsHttpServer = new PrometheusExporterMetricsHttpServer(promExporter);
+            metricsHttpServer.Start();
+
+            var processor = new UngroupedBatcher();
+
+            MeterProvider.SetDefault(Sdk.CreateMeterProviderBuilder()
+                .SetProcessor(processor)
+                .SetExporter(promExporter)
+                .SetPushInterval(TimeSpan.FromSeconds(1))
+                .Build());
+
+            var meterProvider = MeterProvider.Default;
+
+            var meter = meterProvider.GetMeter("MyMeter");
+
+            var testCounter = meter.CreateInt64Counter("MyCounter");
+            var defaultContext = default(SpanContext);
+
+            for (var i = 0; i < 1000; i++)
+            {
+                testCounter.Add(defaultContext, 1, meter.GetLabelSet(new[] { KeyValuePair.Create("my", "test") }));
+                Task.Delay(1_000).Wait();
+            }
+
+            metricsHttpServer.Stop();
+        }
+    }
+}
---
+++ code_examples/Tracing/Program.cs
@@ -0,0 +1,46 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Tracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ServiceName = "Examples.Tracing";
+            var activitySource = new ActivitySource(ServiceName);
+
+            using var openTelemetry = Sdk.CreateTracerProviderBuilder()
+                    .AddSource(ServiceName)
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(ServiceName))
+                    .SetSampler(new AlwaysOnSampler())
+                    // .SetSampler(new TraceIdRatioBasedSampler(0.5))
+                    // .SetSampler(new ParentBasedSampler(new TraceIdRatioBasedSampler(0.5)))
+                    .AddJaegerExporter(options =>
+                        {
+                            options.AgentHost = "localhost";
+                            options.AgentPort = 6831;
+                            options.MaxPayloadSizeInBytes = 65000;
+                        })
+                    .Build();
+
+            using (var activity = activitySource.StartActivity("MyTrace"))
+            {
+                activity?.SetTag("some.tag", "value");
+
+                using (activitySource.StartActivity("ChildTrace"))
+                {
+                    Task.Delay(1_000).Wait();
+                }
+
+                using (activitySource.StartActivity("AnotherChildTrace"))
+                {
+                    Task.Delay(1_000).Wait();
+                }
+            }
+        }
+    }
+}
